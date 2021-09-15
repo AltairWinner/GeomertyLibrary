16bcb1dca0cc6a3565c74ab70a0c5932961f98a7
DmitryGolubenkov <xxdimonxx15@yandex.ru>
2021-09-15 19:42:28 +0300

    Тесты второго конструктора

--- GeometryLibraryUnitTests/PolygonShapeTests.cs
+++ GeometryLibraryUnitTests/PolygonShapeTests.cs
@@ -8,7 +8,7 @@ namespace GeometryLibraryUnitTests
     public class PolygonShapeTests
     {
         [TestMethod]
-        public void TestConstructor()
+        public void TestVerticesConstructor()
         {
             var shape = new PolygonShape(
                 new Vertex[] {
@@ -16,12 +16,12 @@ namespace GeometryLibraryUnitTests
                     new Vertex(0, 3),
                     new Vertex(4, 0)
             });
-            Assert.AreEqual(new Vertex(0, 0).x, shape.vertices[0].x);
-            Assert.AreEqual(new Vertex(0, 0).y, shape.vertices[0].y);
-            Assert.AreEqual(new Vertex(0, 3).x, shape.vertices[1].x);
-            Assert.AreEqual(new Vertex(0, 3).y, shape.vertices[1].y);
-            Assert.AreEqual(new Vertex(4, 0).x, shape.vertices[2].x);
-            Assert.AreEqual(new Vertex(4, 0).y, shape.vertices[2].y);
+            Assert.AreEqual(0d, shape.vertices[0].x);
+            Assert.AreEqual(0d, shape.vertices[0].y);
+            Assert.AreEqual(0d, shape.vertices[1].x);
+            Assert.AreEqual(3d, shape.vertices[1].y);
+            Assert.AreEqual(4d, shape.vertices[2].x);
+            Assert.AreEqual(0d, shape.vertices[2].y);
             try
             {
                 shape = new PolygonShape(
@@ -66,6 +66,63 @@ namespace GeometryLibraryUnitTests
             var nullArray = new Vertex[5]; //Массив из null
             Assert.ThrowsException<ArgumentNullException>(() => new PolygonShape(nullArray),
                 "Массив со значениями null обработан некорректно");
+        }
+
+        [TestMethod]
+        public void TestConstructorTuple()
+        {
+            var shape = new PolygonShape(
+                new (double, double)[] {
+                    (0, 0),
+                    (0, 3),
+                    (4, 0)
+            });
+            Assert.AreEqual(0d, shape.vertices[0].x);
+            Assert.AreEqual(0d, shape.vertices[0].y);
+            Assert.AreEqual(0d, shape.vertices[1].x);
+            Assert.AreEqual(3d, shape.vertices[1].y);
+            Assert.AreEqual(4d, shape.vertices[2].x);
+            Assert.AreEqual(0d, shape.vertices[2].y);
+            try
+            {
+                shape = new PolygonShape(
+                    new (double, double)[] {
+                    (0, 0),
+                    (0, 3),
+                    (3, 4),
+                    (3, 2)
+                });
+
+                shape = new PolygonShape(
+                new (double, double)[] {
+                    (-4, 0),
+                    (-2, -1),
+                    (1, -1),
+                    (2, -2),
+                    (4, 0),
+                    (3, 3),
+                    (-1, 4),
+                    (-2, 2)});
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("При создании корректного полигона возникло исключение: " + e.Message);
+            }
+
+            Assert.ThrowsException<InvalidOperationException>(() => new PolygonShape(
+                new (double, double)[] {
+                    (0, 0),
+                    (0, 3),
+                    (3, 4),
+                    (1, 5) //Из-за этой вершины полигон получается самопересекающимся
+            }), "Создание самопересекающегося полигона обработано некорректно");
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new PolygonShape(
+                new (double, double)[] {
+                    (0, 0),
+                    (1, 5)
+            }), "Создание полигона из двух точек обработано некорректно");
         }
 
         [TestMethod]
